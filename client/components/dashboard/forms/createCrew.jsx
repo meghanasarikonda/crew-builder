ca8a15b163d478a9888449da7f29b77ad3d33994
meghanasarikonda <meghana94987@gmail.com>
2017-11-08 03:44:29 +0530

    redirect

--- client/components/dashboard/forms/createCrew.jsx
+++ client/components/dashboard/forms/createCrew.jsx
@@ -14,12 +14,23 @@ export default class CreateCrew extends Component {
       url: '',
       name: '',
       description: '',
-      image: null
+      image: null,
+      error: null
     };
     // props contain name and unique details of user, so we can keep track of who created this crew  and store it in database accordingly
 
     this.handleSubmit = (e) => {
       e.preventDefault();
+
+      // for (let key in this.state) {
+      //   console.log('hhhh')
+      //   if (this.state[key] === '') {
+      //     console.log('form error ' + key)
+      //     this.setState({error: `The following field is required: ${key}`});
+      //     return;
+      //   }
+      // }
+
       var formData = new FormData();
       formData.append("picture", this.state.image);
 
@@ -33,22 +44,31 @@ export default class CreateCrew extends Component {
         .then((data) => {
           // console.log(data, 'data in line 83')
           this.setState({url: data.message})
-          var obj = {
+          this.obj = {
             name: this.state.name,
             description: this.state.description,
             image: this.state.url
           }
 
-          PostCrew(obj, this.props.user.id, function (err, data) {
+          let crew = this;
+
+          PostCrew(this.obj, this.props.user.id, function (err, data) {
             if (err) {
               console.log('error in posting');
             }
             if (data) {
               props.getCurrentCrews(props.user.id);
               console.log(data, 'data from posting');
+              console.log(data.crew_id, 'datacrewid');
+              // console.log(obj, 'obj crew line 61')
+              // props.setCurrentCrew(obj, 'lead');
+              console.log('heee')
+              props.setCurrentCrew(crew.obj, 'lead')
               //document.getElementById("course-form").reset();
             }
           });
+          console.log(res, 'response in line 68')
+          props.setCurrentCrew(this.obj, 'lead')
         })
         .catch((errors) => {
           console.log('Login Error: ', errors);
@@ -58,11 +78,13 @@ export default class CreateCrew extends Component {
   }
 
   render() {
+    console.log(this.props, 'this.props in line 73')
     return (
       <div>
+      {this.state.error ? <p className="postListing-error">{this.state.error}</p> : ''}
       <form onSubmit={this.handleSubmit}>
-        <FormControl type="text" name="name" value={this.state.name} placeholder="enter name" onChange={(e) => this.setState({name: e.target.value})}/> <br/>
-        <FormControl type="text" name="description" value={this.state.description} placeholder="description" onChange={(e) => this.setState({description: e.target.value})}/> <br/>
+        <FormControl type="text" name="name" value={this.state.name} placeholder="enter name" onChange={(e) => this.setState({name: e.target.value})} required/> <br/>
+        <FormControl type="text" name="description" value={this.state.description} placeholder="description" onChange={(e) => this.setState({description: e.target.value})} /> <br/>
         <FormControl type="file" name="pic" onChange={(e) => this.setState({image: e.target.files[0]})}/><br/>
         <button>submit</button>
       </form>
@@ -70,35 +92,3 @@ export default class CreateCrew extends Component {
     );
   }
 }
-
-
-// <form onSubmit={this.handleSubmitt} id="course-form" ref="form">
-          // <input type="text" name="name" value={this.state.name} placeholder="enter name" onChange={(e) => this.setState({name: e.target.value})}/>
-          // <input type="text" name="description" value={this.state.description} placeholder="description" onChange={(e) => this.setState({description: e.target.value})}/>
-          // <input type="file" name="image" onChange={(e) => this.setState({image: e.target.files[0]})} />
-          // <button>Submit</button>
-//        </form>
-
-//<FormControl type="text" placeholder="Enter the name of Crew" name="crewname" inputRef={ref => this.name = ref} defaultValue={this.props.name}/><br/>
-//           <FormControl componentClass="textarea" placeholder="enter description" inputRef={ref => this.description = ref} defaultValue={this.props.desc}/><br/>
-//            <Dropzone
-              // muliple="false"
-              // accept="image/*"
-              // onDrop={this.onImageDrop.bind(this)}>
-
-            //   {this.state.uploadedFileCloudinaryUrl !== '' ?
-            //     <div>
-            //       <p>{this.state.uploadedFile.name}</p>
-            //       <Image cloudName={cloud_name} publicId={this.state.uploadedFileCloudinaryUrl} width="200" crop="scale"/>
-            //     </div> : <div>
-            //       <p>Drop an image or click to select a file to upload.</p>
-            //     </div>}
-
-            // </Dropzone>
-            // <input type="file" onChange={this.handleFormData}/>
-//            <Button type="submit">
-//               Create/Update
-//            </Button>
-
-
-// <Image cloudName="sarikonda" publicId="flower" width="300" crop="scale"/>
